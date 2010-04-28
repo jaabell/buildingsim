a9ce202b5d6e3423b8829cabb0a61aaabac66a8e
ja.abell <ja.abell@0ebb97cd-3c5c-f44b-770c-6b7441811520>
2010-04-28 15:35:53 +0000

    * Se esta trabajando en envresp (aun no funciona bien pero no genera errores). * Se documenta definitions.py * Se modifican funciones que grafican para que el GUI tenga el control de cuando graficar. * Se cambian tabs por espacios en todos los archivos cambiados.

--- trunk/src/BuildingsimApp/senales.py
+++ trunk/src/BuildingsimApp/senales.py
@@ -1,21 +1,21 @@
 from scipy import *
 #Harmonic or Chirp-Like
 def harmonic(t,a,f=1.,b=0,K=0.,tmin=0.,tmax=Inf,):
-	y = (a*sin(2.*pi*(f*t + K*t**2/2)) + b*cos(f*t + K*t**2/2))*(t>=tmin)*(t<=tmax)
-	return y
+    y = (a*sin(2.*pi*(f*t + K*t**2/2)) + b*cos(f*t + K*t**2/2))*(t>=tmin)*(t<=tmax)
+    return y
 
 #Rectangular
 def rectangular(t,a,t0=0.,f=1.,tmin=0.,tmax=Inf):
-	y = a*sign(sin(2*pi*f*(t-t0)))*(t>=tmin)*(t<=tmax)
-	return y
-	
+    y = a*sign(sin(2*pi*f*(t-t0)))*(t>=tmin)*(t<=tmax)
+    return y
+
 #Triangular
 #def triangular(t,a,t0=0.,f=1.,tmin=0.,tmax=Inf):
-	#t_ = 
-	#y = a*sign(sin(2*pi*f*(t-t0)))*(t>=tmin)*(t<=tmax)
-	#return y	
-	
+    #t_ =
+    #y = a*sign(sin(2*pi*f*(t-t0)))*(t>=tmin)*(t<=tmax)
+    #return y
+
 #def stepsine(t,a,durs,freqs)
-	#N = 
-	#return y
-#def 
+    #N =
+    #return y
+#def
