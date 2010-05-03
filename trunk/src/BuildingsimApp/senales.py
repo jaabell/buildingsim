e3a59dbef78c5fbddcbabeef8c58321f0667ad41
ja.abell <ja.abell@0ebb97cd-3c5c-f44b-770c-6b7441811520>
2010-05-03 22:48:48 +0000

    * Agregué señal tipo ensayo. * Nose que hice en definitions.py pero algo cambió (ver diff). :P

--- trunk/src/BuildingsimApp/senales.py
+++ trunk/src/BuildingsimApp/senales.py
@@ -3,6 +3,24 @@ from scipy import *
 def harmonic(t,a,f=1.,b=0,K=0.,tmin=0.,tmax=Inf,):
     y = (a*sin(2.*pi*(f*t + K*t**2/2)) + b*cos(2.*pi*(f*t + K*t**2/2)))*(t>=tmin)*(t<=tmax)
     return y
+#
+def ensayo_real(t,intervalos,frecuencias,d,phi=0.):
+    tpos = intervalos.cumsum()
+    N = intervalos.size
+    y = zeros(t.shape)
+    for i in range(N):
+        if i == 0:
+            ti = 0
+        else:
+            ti = tpos[i-1]
+        tf = tpos[i]
+        f = frecuencias[i]
+        y += -(2*pi*f)**2*d*sin(2*pi*f*t + phi)*( t >= ti)*(t<= tf)
+        phi += 2*pi*f*intervalos[i]
+        print 'ti = {0:5.1f}, tf = {1:5.1f}, f = {2:6.3f}'.format(ti,tf,f)
+    return y
+        
+
 
 #Rectangular
 def rectangular(t,a,t0=0.,f=1.,tmin=0.,tmax=Inf):
